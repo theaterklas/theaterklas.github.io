c72a3aee7e5b7b4d44cc4225cfe0223298149f1e
mg-1999 <matthijs@voxelvoid.net>
2015-03-17 21:11:03 +0100

    Design changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 $(document).ready(function () {
   var setupRandomImage = function (element) {
-    var $element = $(element);
-    var width = $element.width();
-    var height = $element.outerHeight();
-    var src = 'https://unsplash.it/' + width + '/' + height + '?blur&random&element=' + $element.prop('tagName');
+    var $element, width, height, src;
+    $element = $(element);
+    width = $element.width();
+    height = $element.outerHeight();
+    src = 'https://unsplash.it/' + width + '/' + height + '?blur&random&element=' + $element.prop('tagName');
 
     $element.css('background-image', 'url(' + src + ')');
   };
 
   setupRandomImage('.header');
+  
+  var setupTwitter = function (d, s, id) {
+    var js, fjs, p;
+    fjs = d.getElementsByTagName(s)[0];
+    p = /^http:/.test(d.location) ? 'http' : 'https';
+    
+    if (!d.getElementById(id)) {
+      js = d.createElement(s);
+      js.id = id;
+      js.src = p + "://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }
+  };
+  
+  setupTwitter(document, "script", "twitter-wjs");
 });
+
+/*!function (d, s, id) {
+  var js;
+  var fjs = d.getElementsByTagName(s)[0];
+  var p = /^http:/.test(d.location) ? 'http':'https';
+  if(!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = p + "://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}();*/
