de8ce205975abb07c40ebc288816b5318364dcc7
mg-1999 <mg-h-w@live.nl>
2015-05-07 01:23:26 +0200

    Add twitter card, not ready for use yet

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  'use strict';
   var setupRandomImage = function (element) {
     var $element, width, height, src;
     $element = $(element);
@@ -6,11 +7,9 @@ $(document).ready(function () {
     height = $element.outerHeight();
     image = '304';
     src = 'https://unsplash.it/g/' + width + '/' + height + '?blur&image=' + image + '&element=' + $element.prop('tagName');
-
+    
     $element.css('background-image', 'url(' + src + ')');
   };
-
-  setupRandomImage('.header');
   
   var setupTwitter = function (d, s, id) {
     var js, fjs, p;
@@ -25,29 +24,6 @@ $(document).ready(function () {
     }
   };
   
+  setupRandomImage('header');
   setupTwitter(document, "script", "twitter-wjs");
 });
-
-/* <![CDATA[
-cc.initialise({
-  cookies: {
-    analytics: {},
-    social: {}
-  },
-  settings: {
-    consenttype: "implicit"
-  }
-});
-//]]>
-
-/*!function (d, s, id) {
-  var js;
-  var fjs = d.getElementsByTagName(s)[0];
-  var p = /^http:/.test(d.location) ? 'http':'https';
-  if(!d.getElementById(id)) {
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p + "://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }
-}();*/
